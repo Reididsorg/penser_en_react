a48d5aa5dc82ba54f76aae365a43977e40338293
bruno.grosdidier@2dcom.fr <bruno.grosdidier@2dcom.fr>
2025-08-21 15:13:17 +0200

    Add useState elements to manage interactivity #develop

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const PRODUCTS = [
     { category: "Fruits", price: "1 €", stocked: true, name: "Pomme" },
     { category: "Fruits", price: "1 €", stocked: true, name: "Fruit du dragon" },
@@ -12,20 +14,43 @@ function App() {
 }
 
 function FilterableProductTable({ products }) {
+    const [filterText, setFilterText] = useState('');
+    const [inStockOnly, setInStockOnly] = useState(false);
+
     return (
         <div>
-            <SearchBar />
-            <ProductTable products={products} />
+            <SearchBar
+                filterText={filterText}
+                inStockOnly={inStockOnly}
+                onFilterTextChange={setFilterText}
+                onInStockOnlyChange={setInStockOnly}
+            />
+            <ProductTable
+                products={products}
+                filterText={filterText}
+                inStockOnly={inStockOnly}
+            />
         </div>
     );
 }
 
-function SearchBar() {
+function SearchBar({
+                       filterText,
+                       inStockOnly,
+                       onFilterTextChange,
+                       onInStockOnlyChange
+                   }) {
     return (
         <form>
-            <input type="text" placeholder="Recherche..." />
+            <input
+                type="text"
+                value={filterText} placeholder="Recherche..."
+                onChange={(e) => onFilterTextChange(e.target.value)} />
             <label>
-                <input type="checkbox" />
+                <input
+                    type="checkbox"
+                    checked={inStockOnly}
+                    onChange={(e) => onInStockOnlyChange(e.target.checked)} />
                 {' '}
                 N’afficher que les produits en stock
             </label>
@@ -33,11 +58,21 @@ function SearchBar() {
     );
 }
 
-function ProductTable({ products }) {
+function ProductTable({ products, filterText, inStockOnly }) {
     const rows = [];
     let lastCategory = null;
 
     products.forEach((product) => {
+        if (
+            product.name.toLowerCase().indexOf(
+                filterText.toLowerCase()
+            ) === -1
+        ) {
+            return;
+        }
+        if (inStockOnly && !product.stocked) {
+            return;
+        }
         if (product.category !== lastCategory) {
             rows.push(
                 <ProductCategoryRow
