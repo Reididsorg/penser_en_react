012b12ce38f3ce59182cf3af00826edf96fe2c3b
bruno.grosdidier@2dcom.fr <bruno.grosdidier@2dcom.fr>
2025-08-21 11:42:40 +0200

    Static version of the mock-up in REACT #develop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,93 @@
+function ProductCategoryRow({ category }) {
+    return (
+        <tr>
+            <th colSpan="2">
+                {category}
+            </th>
+        </tr>
+    );
+}
+
+function ProductRow({ product }) {
+    const name = product.stocked ? product.name :
+        <span style={{ color: 'red' }}>
+      {product.name}
+    </span>;
+
+    return (
+        <tr>
+            <td>{name}</td>
+            <td>{product.price}</td>
+        </tr>
+    );
+}
+
+function ProductTable({ products }) {
+    const rows = [];
+    let lastCategory = null;
+
+    products.forEach((product) => {
+        if (product.category !== lastCategory) {
+            rows.push(
+                <ProductCategoryRow
+                    category={product.category}
+                    key={product.category} />
+            );
+        }
+        rows.push(
+            <ProductRow
+                product={product}
+                key={product.name} />
+        );
+        lastCategory = product.category;
+    });
+
+    return (
+        <table>
+            <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Prix</th>
+            </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+        </table>
+    );
+}
+
+function SearchBar() {
+    return (
+        <form>
+            <input type="text" placeholder="Recherche..." />
+            <label>
+                <input type="checkbox" />
+                {' '}
+                N’afficher que les produits en stock
+            </label>
+        </form>
+    );
+}
+
+function FilterableProductTable({ products }) {
+    return (
+        <div>
+            <SearchBar />
+            <ProductTable products={products} />
+        </div>
+    );
+}
+
+const PRODUCTS = [
+    { category: "Fruits", price: "1 €", stocked: true, name: "Pomme" },
+    { category: "Fruits", price: "1 €", stocked: true, name: "Fruit du dragon" },
+    { category: "Fruits", price: "2 €", stocked: false, name: "Fruit de la passion" },
+    { category: "Légumes", price: "2 €", stocked: true, name: "Épinard" },
+    { category: "Légumes", price: "4 €", stocked: false, name: "Citrouille" },
+    { category: "Légumes", price: "1 €", stocked: true, name: "Petits pois" }
+];
+
 function App() {
-  return (
-      'Hello'
-  );
+    return <FilterableProductTable products={PRODUCTS} />;
 }
 
 export default App;
